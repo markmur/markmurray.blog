8af40a27f5474382d49357280e33f8c6a544cf3c
Mark Murray <mark.murray@shopify.com>
2022-04-01 11:19:25 +0100

    Better heading

--- src/pages/photography/film.tsx
+++ src/pages/photography/film.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import Layout from '../../components/Layout';
-import { Box, Text, Container, PageHeading } from '../../styles';
+import {
+  Box,
+  Text,
+  Container,
+  PageHeading,
+  Description,
+  PostTitle,
+  Subtitle,
+} from '../../styles';
 import Masonry from 'react-masonry-css';
 import { graphql } from 'gatsby';
 
@@ -41,15 +49,22 @@ function Photos(props) {
         }
         `}
       </style>
-      <Container>
-        <Box mb={2}>
-          <Box mb={4}>
-            <PageHeading>Film Photography</PageHeading>
-          </Box>
-          <Box mb={2}>
-            <Text as="small">Lomography Redscale XP 50-200 (shot at 400)</Text>
-          </Box>
+
+      <Container textAlign="center">
+        <Box mt={4}>
+          <Subtitle>Film Photography</Subtitle>
         </Box>
+
+        <PostTitle mb={3}>Lomo Redscale XP</PostTitle>
+
+        <Box maxWidth={800} margin="auto">
+          <Description>
+            A collection of photos taken in Dublin with a Canon AE-1 and
+            Lomography Redscale XP 50-200.
+          </Description>
+        </Box>
+
+        <hr />
 
         <Masonry
           breakpointCols={{ default: 3, 1100: 3, 700: 2, 500: 1 }}
