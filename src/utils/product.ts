ba666ec066f2dffe735f31a0535b885f74d838a2
Mark Murray <mark.murray@shopify.com>
2022-04-01 11:10:43 +0100

    Add size helpers

--- src/utils/product.ts
+++ src/utils/product.ts
@@ -44,3 +44,38 @@ export function getProductUrl({
 
   return `/photography/${handle}`;
 }
+
+const space = '\\s?'
+const inch = '(in|inches)'
+const formatNum = (num: string) => num.replace(/\./g, '\\.')
+
+const buildRegex = (cm: [string, string], inches: [string, string]) => {
+  return new RegExp(
+    `^${formatNum(cm[0])}${space}cm${space}x${space}${formatNum(cm[1])}${space}cm${space},${space}${formatNum(inches[0])}${space}${inch}${space}x${space}${formatNum(inches[1])}${space}${inch}$`,
+    'i'
+  )
+}
+
+export const RE_A2 = buildRegex(['42.0', '59.4'], ['16.5', '23.4'])
+export const RE_A3 = buildRegex(['29.7', '42.0'], ['11.7', '16.5'])
+export const RE_A4 = buildRegex(['21.0', '29.7'], ['8.3', '11.7'])
+
+export enum Sizes {
+  A2 = "42.0 cm x 59.4 cm, 16.5 inches x 23.4 inches",
+  A3 = "29.7 cm x 42.0 cm, 11.7 inches x 16.5 inches",
+  A4 = "21.0 cm x 29.7 cm, 8.3 inches x 11.7 inches"
+}
+
+export function dimensionsToVariantSize(dimensions: string) {
+  const sizes = Object.entries(Sizes)
+
+  const match = sizes.find(([key, value]) => value === dimensions)
+
+  if (match) {
+    const [key, value] = match
+    return `${key} (${value})`
+  }
+
+  return dimensions
+}
+
