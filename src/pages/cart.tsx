d5533f34717e03a593b76152d35ca23e1235d7ed
Mark Murray <mark.murray@shopify.com>
2022-07-22 10:31:11 +0100

    Add container to cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Cart from '../components/Cart';
 import Layout from '../components/Layout';
+import { Container } from '../styles';
 
 const CartPage = () => {
   return (
     <Layout displayTagline={false}>
-      <Cart open />
+      <Container>
+        <Cart open />
+      </Container>
     </Layout>
   );
 };
